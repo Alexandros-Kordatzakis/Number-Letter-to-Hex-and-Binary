89832903f50343c25ed36dc2d9882a103d300ee8
Alexandros Kordatzakis <alexkordatzakis2@outlook.com.gr>
2018-06-24 17:26:21 +0300

    Added all files.

---
+++ Number-Letter-to-Hex-and-Binary/Number-Letter-to-Hex-and-Binary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace testappforuserinput {
+
+    class Program {
+
+        static void Main() {
+
+            while (true) {
+
+                Console.WriteLine("\nEnter a letter or number: ");
+                var userInput = Console.ReadLine();
+                var letterAsNum = (int)userInput[0];
+
+                System.Threading.Thread.Sleep(1000);
+
+                Console.WriteLine("Decimal: {0:d}", letterAsNum);
+                // System.Threading.Thread.Sleep(500);
+                Console.WriteLine("Hexadecimal: {0:x2}", letterAsNum);
+
+                Console.WriteLine("\n********************************");
+
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
